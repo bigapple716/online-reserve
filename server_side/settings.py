fa0253cf4fedfffdd6ce3fceb49b2dc61909e2f5
mikemhzhang <mikemhzhang@tencent.com>
2019-08-18 17:19:08 +0800

    another try to display css

--- server_side/settings.py
+++ server_side/settings.py
@@ -119,8 +119,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = './static/'
-STATICFILES_DIRS = [
-    './catalog/static/',
-]
+STATIC_URL = os.path.join(BASE_DIR, "static/"),
+STATIC_ROOT = os.path.join(BASE_DIR, "static/"),
